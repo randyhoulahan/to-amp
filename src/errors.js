d68e9cc43abc77b19aabff2c4736d09efab381f6
Randy Houlahan <randy.houlahan@cbd.int>
2019-10-06 12:39:44 -0400

    cleaning

--- src/errors.js
+++ src/errors.js
@@ -1,30 +1,30 @@
 import ExtendableError from 'es6-error'
 import consola from 'consola'
 
-export const NoBody = class extends ExtendableError {
-  constructor (message = 'Error: no body tags found in html string') {
+export const NoBody = class extends ExtendableError{
+  constructor (message = 'Error: no body tags found in html string'){
     super(message)
     consola.error(message, this)
   }
 }
 
-export const HtmlNotString = class extends ExtendableError {
-  constructor (message = 'Error: html provided is not a string') {
+export const HtmlNotString = class extends ExtendableError{
+  constructor (message = 'Error: html provided is not a string'){
     super(message)
     consola.error(message, this)
   }
 }
 
-export const AmpImgAttrs = class extends ExtendableError {
-  constructor (l1, l2, message = '') {
+export const AmpImgAttrs = class extends ExtendableError{
+  constructor (l1, l2, message = ''){
     message = `Error: Additional amp-img attributes array not the legth of the loaded images array /n addlAttribsArr:${l1} != ${l2}`
     super(message)
     consola.error(message, this)
   }
 }
 
-export const NotObjError = class extends ExtendableError {
-  constructor (message = 'Error: Additional amp-img attributes contains a non object') {
+export const NotObjError = class extends ExtendableError{
+  constructor (message = 'Error: Additional amp-img attributes contains a non object'){
     super(message)
     consola.error(message, this)
   }
